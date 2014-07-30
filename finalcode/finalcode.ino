1b2e2a067da224503526e180808a13d27d32bad6
Mei <maylaures@gmail.com>
2014-07-30 15:10:15 -0700

    update

--- finalcode/finalcode.ino
+++ finalcode/finalcode.ino
@@ -1,11 +1,16 @@
 const int button1Pin = 2;  // pushbutton 1 pin
 const int button2Pin = 3;  // pushbutton 2 pin
+const int button3Pin = 4;  // pushbutton 3 pin
 const int ledPin =  13;    // LED pin
 
-const int buzzerPin = 9;
-const int songLength = 18;
-char notes[] = "cdfda ag cdfdg gf "; // a space represents a rest
-int beats[] = {1,1,1,1,1,1,4,4,2,1,1,1,1,1,1,4,4,2};
+int message[1000];
+int counter = 0;
+int none = 0;
+
+const int buzzerPin = 9;   // buzzer pin
+const int songLength = 3; // length
+char notes[] = "cdf"; // a space represents a rest
+int beats[] = {1,1,1};
 int tempo = 150;
 
 void setup()
@@ -14,9 +19,10 @@ void setup()
   // Set up the pushbutton pins to be an input:
   pinMode(button1Pin, INPUT);
   pinMode(button2Pin, INPUT);
-
+  pinMode(button3Pin, INPUT);
+  
   // Set up the LED pin to be an output:
-  pinMode(ledPin, OUTPUT);  
+  pinMode(ledPin, OUTPUT);
 
 //9
  pinMode(buzzerPin, OUTPUT);  
@@ -28,7 +34,7 @@ void loop()
   //9
   int i, duration;
   
-  for (i = 0; i < songLength; i++) // step through the song arrays
+ /* for (i = 0; i < songLength; i++) // step through the song arrays
   {
     duration = beats[i] * tempo;  // length of note/rest in ms
     
@@ -42,23 +48,49 @@ void loop()
       delay(duration);            // wait for tone to finish
     }
     delay(tempo/10);              // brief pause between notes
-  }
+  }*/
   
-   while(true){
-     int button1State, button2State;  // variables to hold the pushbutton states
+  // while(true){
+     int button1State, button2State, button3State;  // variables to hold the pushbutton states
      button1State = digitalRead(button1Pin);
      button2State = digitalRead(button2Pin);
-     if (((button1State == LOW) || (button2State == LOW))  // if we're pushing button 1 OR button 2
-        && !                                               // AND we're NOT
-        ((button1State == LOW) && (button2State == LOW))) // pushing button 1 AND button 2                                                     // then...
-     {
-        digitalWrite(ledPin, HIGH);  // turn the LED on
+     button3State = digitalRead(button3Pin);
+     
+     if (button1State == LOW && (button2State == HIGH && button3State == HIGH)) {
+       digitalWrite(ledPin, HIGH);
+       delay(200);
+       digitalWrite(ledPin, LOW);
+       message[counter] = 0;
+       counter++;
+       none = 0;
      }
-     else
-     {
-        digitalWrite(ledPin, LOW);  // turn the LED off
+     
+     if (button2State == LOW && (button1State == HIGH && button3State == HIGH)) {
+       digitalWrite(ledPin, HIGH);
+       delay(1000);
+       digitalWrite(ledPin, LOW);
+       message[counter] = 1;
+       counter++;
+       none = 0;
      }
-   }
+     
+     if (button3State == LOW  && (button1State == HIGH && button2State == HIGH)) {
+       digitalWrite(ledPin, LOW);
+       message[counter] = -1;
+       counter++;
+       none = 0;
+     }
+     
+     while (button1State == HIGH && button2State == HIGH && button3State == HIGH) {
+       none++;
+       delay(1000);
+     }
+     
+     if (none > 3) {
+       
+     }
+   
+   //  }
   }
   
 int frequency(char note) 
